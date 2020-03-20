5f7b747d0fbd3686a55ba9d2c33a705bbeab132c
Zak <4021809+Soxs@users.noreply.github.com>
2020-03-20 12:21:09 +0000

    Add files via upload

---
+++ RLMiner.java
@@ -0,0 +1,85 @@
+import com.runeliteplus.Utils;
+import net.runelite.api.GameObject;
+import net.runelite.client.pluginsplus.soxs.rlbot.api.APIUtils;
+import net.runelite.client.pluginsplus.soxs.rlbot.api.input.Mouse;
+import net.runelite.client.pluginsplus.soxs.rlbot.api.scripting.RLScript;
+import net.runelite.client.pluginsplus.soxs.rlbot.api.scripting.RLScriptDescriptor;
+
+import java.awt.*;
+
+@RLScriptDescriptor(
+        name = "RLMiner",
+        description = "A miner that supports banking and power-levelling. Can fill inventory or mine sceneX drop sceneX.",
+        version = 0.01,
+        author = "Soxs",
+        requiresAuthentication = true
+)
+public class RLMiner extends RLScript {
+
+    private GameObject selectedOre = null;
+    private int[] ORE_IDS = {11361};
+    private int[] INVENTORY_ID = {438};
+
+    private final Color shapeColor = new Color(255, 89, 0, 109);
+    private Shape oreArea = null;
+
+    private RLMinerGUI gui;
+
+    @Override
+    public boolean onStart() {
+        System.out.println("Starting RLMiner!");
+
+        EventQueue.invokeLater(() -> gui = new RLMinerGUI());
+
+        while (gui == null || !gui.isSet)
+            sleep(1000, 1550);
+
+        ORE_IDS = gui.selectedOre;
+        INVENTORY_ID = gui.selectedInventory;
+
+        gui = null;
+
+        return true;
+    }
+
+    @Override
+    public void onEnd() {
+        System.out.println("Stopping RLMiner...");
+    }
+
+    @Override
+    public int loop() {
+        if (getInventory().getItemCount(INVENTORY_ID) >= 3 ) {
+            dropOres();
+        } else {
+            findAndMineOre();
+            return Utils.randInt(4007, 5007); //longer rests.
+        }
+
+        return Utils.randInt(2000, 4000); //Rest for a random amount of time.
+    }
+
+    private void findAndMineOre() {
+        selectedOre = APIUtils.getNearestObject(context, getGameObjects(ORE_IDS)); //Get the closest tree.
+        oreArea = selectedOre.getClickbox(); //Save the click box of the tree so we can paint our target.
+
+        if (selectedOre != null && APIUtils.isPlayerIdle(context)) { //Ensure the tree is valid, and only proceed if our player is idle.
+            Mouse.moveAndClick(this, APIUtils.getPointInObject(selectedOre)); //Move the mouse and click the tree.
+        }
+        else {
+            if (Utils.randInt(0, 100) > Utils.randInt(55, 65))
+                Mouse.moveToSuitableRestArea(this);
+        }
+    }
+
+    private void dropOres() {
+        if (getInventory().getItemCount(INVENTORY_ID) > 0)
+            Mouse.moveAndShiftClick(this, getInventory().getInventoryClickPoint(INVENTORY_ID));
+    }
+
+    @Override
+    public void overlay(Graphics2D graphics) {
+        graphics.setColor(shapeColor); //Set the paint colour to green.
+        graphics.fill(oreArea); //Draw the hit box of the tree.
+    }
+}
---
+++ RLMinerGUI.java
@@ -0,0 +1,69 @@
+package net.runelite.client.pluginsplus.soxs.rlbot.scripts.mining;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RLMinerGUI extends JFrame {
+
+    public int[] selectedOre;
+    public int[] selectedInventory;
+
+    public JTextField ore_id = new JTextField();
+    public JTextField inventory_id = new JTextField();
+    public JButton start = new JButton("Start");
+
+    public boolean isSet = false;
+
+    public RLMinerGUI() {
+        super();
+        setTitle("RLMiner");
+        setLayout(new BorderLayout());
+
+        start.addActionListener((e) -> {
+            String[] ints = ore_id.getText().split(",");
+            selectedOre = new int[ints.length];
+            int i = 0;
+            for (String s : ints)
+            {
+                selectedOre[i] = Integer.valueOf(s);
+                i++;
+            }
+
+            String[] ints2 = inventory_id.getText().split(",");
+            selectedInventory = new int[ints2.length];
+            int i2 = 0;
+            for (String s : ints)
+            {
+                selectedInventory[i2] = Integer.valueOf(s);
+                i2++;
+            }
+            isSet = true;
+            setVisible(false);
+        });
+
+        add(new JPanel() {
+            {
+                setLayout(new BoxLayout(getPanel(), BoxLayout.Y_AXIS));
+
+                add(new JLabel("Ore GameObject ID"));
+                add(ore_id);
+                add(new JLabel("Ore Inventory ID"));
+                add(inventory_id);
+            }
+
+            private JPanel getPanel() {
+                return this;
+            }
+        }, BorderLayout.CENTER);
+
+        add(start, BorderLayout.SOUTH);
+
+        setPreferredSize(new Dimension(400, 150));
+        setResizable(false);
+        setLocationRelativeTo(null);
+        pack();
+        setVisible(true);
+    }
+
+
+}
